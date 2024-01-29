22e2a6ac4f8e5f1de0b38ac0b560faea72623fcb
Nigel <nigel.sibbert@gmail.com>
2024-01-29 07:35:36 +0000

    updated

---
+++ exercise.webapi/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.webapi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        
+        public int AuthorId { get; set; }
+    }
+}
